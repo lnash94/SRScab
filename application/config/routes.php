af938b8c23f0c8837fd0d9840838c424a99431ec
Dula <rdulmina@gmail.com>
2017-10-05 15:59:11 +0530

    fixed header bug in signup page

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['users/login']='users/login';
+$route['(:any)/(:any)']='pages/view/$2';
 $route['(:any)']='pages/view/$1';
 
 $route['default_controller'] = 'pages/view';

f3ed5caa8f3df48c6af70c7108a6715fb9b4c9ec
Sumudu Nissanka <sumudunissanke@gmail.com>
2017-10-19 15:01:18 +0530

    login front end

--- application/views/Customer/login.php
+++ application/views/Customer/login.php
@@ -3,42 +3,6 @@
 <!-- container -->
 	<div class="container">
 
-		<ol class="breadcrumb">
-			<li><a href="index.html">Home</a></li>
-			<li class="active">User access</li>
-		</ol>
-
-		<div class="row">
-
-			<!-- Article main content -->
-			<article class="col-xs-12 maincontent">
-				<header class="page-header">
-					<h1 class="page-title">Sign in</h1>
-				</header>
-
-				<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<h3 class="thin text-center">Sign in to your account</h3>
-							<p class="text-center text-muted">Lorem ipsum dolor sit amet, <a href="signup.html">Register</a> adipisicing elit. Quo nulla quibusdam cum doloremque incidunt nemo sunt a tenetur omnis odio. </p>
-							<hr>
-
-							<form>
-								<div class="top-margin">
-									<label>Username/Email <span class="text-danger">*</span></label>
-									<input type="text" class="form-control">
-								</div>
-								<div class="top-margin">
-									<label>Password <span class="text-danger">*</span></label>
-									<input type="password" class="form-control">
-
-
-
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
 			<div class="col-md-4 col-md-offset-7 col-sm-8 col-sm-offset-2 ">
 					<div class="panel panel-default">
 						<div class="panel-body">
@@ -70,11 +34,11 @@
 							</form>
 						</div>
 					</div>
-                </div>
-            </article>
+             </div>
+//            </article>
 		<!-- /Article -->
         </div>
-	</div>	<!-- /container -->
+//	</div>	<!-- /container -->
 
 
 <?php form_close();?>

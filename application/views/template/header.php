4dc431afbb4448693726878ec801457100f63718
Dula <rdulmina@gmail.com>
2017-10-19 18:24:18 +0530

    login modal added

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/font-awesome.min.css">
       <!-- JQuery -->
 	<script src="<?php echo base_url();?>/assets/js/jquery-3.1.1.min.js"></script>
+   <script src="<?php echo base_url();?>/assets/js/bootstrap.js"></script>
     <!-- Custom styles for our template -->
     <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap-theme.css" media="screen" >
     <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/main.css">
@@ -27,6 +28,39 @@
 </head>
 
 <body class="home">
+<!--signin modal-->
+ <div class="modal fade" id="loginModal">
+         <div class="modal-dialog">
+         <div class="modal-content">   
+    <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<h3 style="color: black;font-style: italic;font-weight:500"><img src="<?php echo base_url();?>assets/images/logo.jpg" alt="Progressus HTML5 template"> </h3>
+          </div>
+          <div class="modal-body">
+            <form method="post" action="php/user.php" name="login_form">
+             <div class="form-group has-feedback">
+        		<label>Username/Email <span class="text-danger">*</span></label>
+				<input type="text" class="form-control" name="username">
+      		</div>
+             <div class="form-group has-feedback">
+        		<label>Password <span class="text-danger">*</span></label>
+				<input type="password" class="form-control" name="password">
+      		</div>
+  
+              <p><button type="submit" class="btn btn-xs btn-primary">Sign in</button>
+                <a  style="color: black;font-style: italic;font-weight:500" href="#">Forgot Password?</a>
+              </p>
+            </form>
+          </div>
+          <div class="modal-footer">
+               <div class="pull-left">New cusomer?<a  style="color: mediumaquamarine;font-style: italic;font-weight:500" href="#">Register here</a></div>
+                <button type="button" class="btn btn-warning btn-xs pull-right" data-dismiss="modal">Close</button>
+                
+              </div>
+			 </div>
+	  </div>
+	  </div>
+
 <!-- Fixed navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top headroom" >
     <div class="container">
@@ -61,7 +95,7 @@
                 <li><a href="<?php echo base_url();?>users/customerpayment">Payment Test</a></li>
                 <li><a href="<?php echo base_url();?>about">About us</a></li>
                 <li><a href="<?php echo base_url();?>contact">Contact us</a></li>
-                <li><a href="<?php echo site_url('users/login') ?>" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">SIGN IN / SIGN UP</a></li>
+                 <button  data-toggle="modal" class="btn btn-xs btn-warning" data-target="#loginModal" >Sign In</button><!--signin model is defined at the top -->
             </ul>
         </div><!--/.nav-collapse -->
     </div>

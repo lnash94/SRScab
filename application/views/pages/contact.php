f9668a0c5f543fe349834ac2f6f652ca85983abc
Dula <rdulmina@gmail.com>
2017-10-20 22:03:37 +0530

    contact us page frontend completed

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -1,2 +1,67 @@
-<div style="margin-top: 100px;"></div>
-Write your contact page here
+<header id="contactuscover">
+</header>
+<br><br>
+
+    <div class="row">
+        <div class="col-md-5  pull-left" style="padding-left: 80px;">
+           <div class="well well-sm">
+           <i class="fa fa-phone  fa-2x" aria-hidden="true"></i><strong style="font-size: 20px; margin-left: 10px;">0112884598</strong><br><strong style="margin-left: 33px;font-size: 20px;">0771567974</strong>
+           
+			</div>
+           <div class="well well-sm">
+           <i class="fa fa-envelope fa-2x" aria-hidden="true"></i><strong style="font-size: 20px; margin-left: 10px;">srscabs@gmail.com</strong>
+           </div>
+            <div class="well well-sm">
+                <form>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="name">
+                                Name</label>
+                            <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                Email Address</label>
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                        </div>
+                        <div class="form-group">
+                            <label for="subject">
+                                Subject</label>
+                            <select id="subject" name="subject" class="form-control" required="required">
+                                <option value="na" selected="">Choose One:</option>
+                                <option value="service">General Customer Service</option>
+                                <option value="suggestions">Suggestions</option>
+                                <option value="product">Product Support</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="name">
+                                Message</label>
+                            <textarea name="message" id="message" class="form-control" rows="6" cols="25" required="required"
+                                placeholder="Message"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
+                            Send Message</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-4">
+            
+           <i class="fa fa-building" aria-hidden="true"></i>
+           <!--google map location of SKM-->          
+          <div id="map" class="pull-right col-md-5" data-aos="fade-up"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15554.8295513227!2d77.6153998!3d12.9265189!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4b30df2716780537!2sSRS+Cabs+Tours+%26+Travels!5e0!3m2!1sen!2slk!4v1508515573035" width="700" height="600" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+            
+                
+           
+        </div>
+</div>

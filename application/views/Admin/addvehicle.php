01bbfe8b67da0a123f5783af45c23b59a22c3e31
Dula <rdulmina@gmail.com>
2017-10-23 00:14:04 +0530

    Admin manage vehicles frontend completed

--- application/views/Admin/addvehicle.php
+++ application/views/Admin/addvehicle.php
@@ -40,16 +40,20 @@
 		<input type="text" class="form-control" name="model" id="vmodel" placeholder="Enter vehicle model" required></br>
 		<label>Number of seats</label>
 		<input type="text" class="form-control" name="seats" id="seats" placeholder="Enter number of seats" required></br>
-		<label style="margin-right: 15px;">Transmission<br> Type Auto</label>
+		<label style="margin-right: 15px;">Transmission Type Auto</label>
 		<label class="switch">
-  		<input type="checkbox" name="Transmission" id="checkboxtogal1" value="false">
+  		<input type="checkbox" name="Transmission" id="ttype" value="false">
   		<span class="slider round"></span>
-		</label></br></br>
-		<label style="margin-right: 15px;">With driver only</label>
+		</label>
+		<label style="margin-right: 15px; margin-left: 80px;">With driver only</label>
 		<label class="switch">
   		<input type="checkbox" name="withdriver" id="checkboxtogal" value="false">
   		<span class="slider round"></span>
 		</label></br></br>
+		<label>Rate per Day(80km)</label>
+		<input type="text" class="form-control" name="rpd" id="rpd" placeholder="Enter rate per day" ></br>
+		<label>Excess mileage charge</label>
+		<input type="text" class="form-control" name="emc" id="emc" placeholder="Enter Excess mileage charge" required></br>
 		<label>Other details</label>
 		<input type="text" class="form-control" name="details" id="details" placeholder="Enter details such as A/C,CD player etc."></br>
 		<button type="button" class="btn btn-success btn-md" id="submitbtn">Register</button>
@@ -78,8 +82,16 @@
 			document.getElementById("checkboxtogal").value="False";
 		}
 	});
-</script>
-<script>
+	$('#ttype').on('change',function(){
+		
+		if(this.checked){
+			
+			document.getElementById("ttype").value="True";
+		}
+		else{
+			document.getElementById("ttype").value="False";
+		}
+	});
 	
 	$('#submitbtn').click(function(){
 		var type=$('#vtype').val();
@@ -87,14 +99,16 @@
 		var brand=$('#brand').val();
 		var vmodel=$('#vmodel').val();
 		var seats=$('#seats').val();
+		var ttype=$('#ttype').val();
+		var rpd=$('#rpd').val();
+		var emc=$('#emc').val();
 		var details=$('#details').val();
 		var withdriver=$('#checkboxtogal').val();
 		var imageLink=$('#imageLink').val();
-		//alert(withdriveronly);
 
 				$.ajax({
 					type:'post',
-					data:{'type':type,'licenno':licenno,'brand':brand,'model':vmodel,'seats':seats, 'details':details,'withdriver':withdriver,'imageLink':imageLink},
+					data:{'type':type,'licenno':licenno,'brand':brand,'model':vmodel,'seats':seats, 'details':details,'withdriver':withdriver,'imageLink':imageLink,'ttype':ttype,'rpd':rpd,emc:emc},
 					url:'<?php echo base_url('Addvehicle/add')?>',
 					success:function(data){
 						if($('#alert')!=null){
@@ -103,15 +117,16 @@
 						if(data=="success"){
 							$("<div id=\"alert\" class=\"alert alert-success col-md-10 col-md-offset-1\"><strong>Success!</strong>New Vehicle Successfully added to the system</div>").insertAfter('#pheader');
 							
-							('#licenno').val()="";
+							$('#licenno').val()="";
 							
-							('#vmodel').val()="";
-							('#seats').val()="";
-							('#details').val()="";
+							$('#vmodel').val()="";
+							$('#seats').val()="";
+							$('#details').val()="";
 							//('#imageLink').val();
 						}
 						else{
 						$("<div id=\"alert\" class=\"alert alert-danger col-md-10 col-md-offset-1\"><strong>Error!</strong>"+data+"</div>").insertAfter('#pheader');
+							window.scrollTo(0,0);
 						}
 					}
 				});
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,7 +9,8 @@
     <meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
 
     <title>SRScabs</title>
-
+	 <!-- JQuery -->
+	<script src="<?php echo base_url();?>/assets/js/jquery-3.1.1.min.js"></script>
     <link rel="shortcut icon" href="<?php echo base_url();?>/assets/images/gt_favicon.png">
     <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap-theme.css" media="screen" >
     <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
@@ -20,8 +21,7 @@
     <link  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/css/bootstrapValidator.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/js/bootstrapValidator.min.js"></script>
 
-    <!-- JQuery -->
-	<script src="<?php echo base_url();?>/assets/js/jquery-3.1.1.min.js"></script>
+   
     <script src="<?php echo base_url();?>/assets/js/bootstrap.js"></script>
     <script src="<?php echo base_url();?>/assets/js/customer_registration.js"></script>
     <!-- Custom styles for our template -->

e7ddb3d125f9df2f3f025306b0798a6d7cc70a11
Unknown <lalangaariyasinghe@gmail.com>
2017-12-17 14:39:51 +0530

    Location Map Added

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -105,6 +105,7 @@
                 </li>
                 <li><a href="<?php echo base_url();?>users/admindashbord">Admin test</a></li>
                 <li><a href="<?php echo base_url();?>users/newdriver">New Driver test</a></li>
+
                 <?php if ($this->session->userdata('logged_in')):?>
                 <li><a href="<?php echo base_url();?>users/customerpayment">Payment Test</a></li>
                 <?php endif;?>

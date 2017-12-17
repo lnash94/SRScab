63c04aa48a17ed1d29c0152a093e770c2493a515
Dula <rdulmina@gmail.com>
2017-12-18 01:08:10 +0530

    upcomming reservations completed

--- application/views/Admin/Manage_reservations.php
+++ application/views/Admin/Manage_reservations.php
@@ -5,13 +5,17 @@
 	border-right: 2px solid #C5C5C5;
 	
 }
-#newreservation{
+#upcommingreservations{
+		border-bottom: 2px solid #C5C5C5;
+	padding-bottom: 15px;
+	}	
+.newreservation{
   list-style-type: none;
 	margin: 0px;
 	padding: 0px;
 }
 
-#newreservation a li{
+.newreservation a li{
 	text-decoration: none;
 	color: #000000;
 	padding-top: 12px;
@@ -23,15 +27,15 @@
 	border: 1px solid #D4D4D4;
 	-webkit-transition: height .5s;
 }
-#newreservation a li:hover{
+.newreservation a li:hover{
 	
 	height: 80px;
 	background-color: #DCDCDC;
 }
-#newreservation a{
+a{
 	text-decoration: none;
 		
-	}
+}
 
 @keyframes fadeIn {
     from {opacity:0;}
@@ -41,7 +45,7 @@
     from {opacity:1;}
     to {opacity:0;}
 }
-#newreservation a li {
+.newreservation a li {
     animation: fadeIn 1.5s;
 }
 }
@@ -57,14 +61,45 @@
 					<h1 class="page-title">Manage reservations</h1>
 		</header>
 <div class="col-md-3" id="newreservationsdiv">
-<ul id="newreservation">
+<div id="upcommingreservations">
+	<strong>Upcomming reservations <span class="label label-success" id="notificationlable2"></span></strong></br></br>
+<ul class="newreservation">
 
 <?php
-				foreach($newservations as $newreservation){//show details about newreservation
+	$upcommingcount=0;
+				foreach($newservations as $newreservation){//show details about upcomming reservations
+					 //$exatrctdate=substr($newreservation['start_Date'],0,10);
+					$exatracttime=strtotime($newreservation['start_Date']);
+					$diff=$exatracttime-time();
+					if(time()<$exatracttime && $diff<(60*60*24*3)){
+						$upcommingcount++;
+					
 				echo("
                   
                     <a href=\"#\"><li value=\"".$newreservation['reservation_No']."\">
-                      New reservation from ".$newreservation['customer_fname']." ".$newreservation['customer_lname']."
+                      reservation from ".$newreservation['customer_fname']." ".$newreservation['customer_lname']." on ".$newreservation['start_Date']."
+                    </li></a>
+                  ");
+						if (($key = array_search($newreservation, $newservations)) !== false) {
+    				unset($newservations[$key]);
+								}
+						
+					}
+					  
+				}
+                 ?>
+</ul>
+</div>
+<div id="otherreservations">
+</br><strong>Other reservations </strong></br></br>
+<ul class="newreservation">
+
+<?php
+				foreach($newservations as $newreservation){//show details about  other reservation
+				echo("
+                  
+                    <a href=\"#\"><li value=\"".$newreservation['reservation_No']."\">
+                      reservation from ".$newreservation['customer_fname']." ".$newreservation['customer_lname']." on ".$newreservation['start_Date']."
                     </li></a>
                   ");
 					  
@@ -72,17 +107,21 @@
                  ?>
 </ul>
 </div>
+</div>
 <div class="col-md-9 col-sm-12 " id="qrdetails">
         <!-- newreservation details will load here--> 
         <center><h3 style="margin-top: 50px;">Select a reservation to view detatils</h3></center>
 </div>		
 </div>	
 <script>
-var frgfr;
-$('#newreservation a li').click(function(){//element to be click to load the page in the div
+$('.newreservation a li').click(function(){//element to be click to load the page in the div
 	var rno=$(this).val();
 	
     $('#qrdetails').load('<?php echo base_url();?>Users/load_manage_reservation_details/'+rno);
 
 });
+$(document).ready(function(){
+	$('#notificationlable2').html("<?php echo $upcommingcount;?>");
+});	
+
 </script>	
